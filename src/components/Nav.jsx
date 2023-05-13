b93ef49f186e05f76da195f82222412456c9d5b7
unknown <chochelius@gmail.com>
2023-05-12 23:12:07 -0400

    second commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,8 +10,8 @@ const Nav = ({onSearchChange}) => {
 
 
                 <form className="d-flex" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" onChange={handleSearchChange}/>
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" onChange={handleSearchChange}/>
+                        <button className="btn btn-outline-success" type="submit">Buscar</button>
                 </form>
             </div>
         </nav>
